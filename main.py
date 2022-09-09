14311379565edaa7b2f38bc7789689cc76516024
prajeshElEvEn <prajesh.eleven118@gmail.com>
2022-09-10 03:40:15 +0530

    - added url and soup

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
+
+site_url = 'https://www.youtube.com/'
+site_html = requests.get(site_url).text
+# print(site_html)
+soup = BeautifulSoup(site_html, 'lxml')
+print(soup.prettify())
+# videos = soup.find('div', class_ = 'style-scope ytd-rich-grid-renderer')
+# print(videos)
