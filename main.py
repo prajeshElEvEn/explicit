3fdcc110526b0d834d0ad851d47b0b81654ef8e6
prajeshElEvEn <prajesh.eleven118@gmail.com>
2022-09-18 19:32:37 +0530

    - commit

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import requests
 
 site_url = 'https://www.youtube.com/'
 site_html = requests.get(site_url).text
-# print(site_html)
-soup = BeautifulSoup(site_html, 'lxml')
-print(soup.prettify())
+print(site_html)
+# soup = BeautifulSoup(site_html, 'lxml')
+# print(soup.prettify())
 # videos = soup.find('div', class_ = 'style-scope ytd-rich-grid-renderer')
 # print(videos)
