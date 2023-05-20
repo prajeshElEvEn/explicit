99db9c7ab1601c47dc793f0ad59b0501f1dda673
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-05-20 15:33:17 +0530

    - written script

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-from bs4 import BeautifulSoup
+import os
 import requests
+from bs4 import BeautifulSoup
+import pandas as pd
 
-site_url = 'https://www.youtube.com/'
-site_html = requests.get(site_url).text
-print(site_html)
-# soup = BeautifulSoup(site_html, 'lxml')
-# print(soup.prettify())
-# videos = soup.find('div', class_ = 'style-scope ytd-rich-grid-renderer')
-# print(videos)
+os.makedirs('explicit_links', exist_ok=True)
+path = os.path.join('explicit_links', 'links.csv')
+
+
+def scrape_youtube_videos(query):
+    url = f"https://www.youtube.com/results?search_query={query}"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    video_links = []
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        if href.startswith('/watch?v='):
+            video_links.append('https://www.youtube.com' + href)
+
+    df = pd.DataFrame({'Video Links': video_links})
+    df.to_csv(path, index=False)
+
+
+q = input('Enter the query:\n> ')
+scrape_youtube_videos(q)
