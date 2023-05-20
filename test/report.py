b4d9ce21d5f96dbb8ea47abda9248a122b30cdf4
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-05-21 02:38:05 +0530

    - fixed reporting - added oauth

---
+++ test/report.py
@@ -0,0 +1,36 @@
+import os
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+
+scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
+
+
+def main():
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    api_service_name = "youtube"
+    api_version = "v3"
+    client_secrets_file = "../client_secret.json"
+
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        client_secrets_file, scopes)
+    credentials = flow.run_console()
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, credentials=credentials)
+
+    request = youtube.videos().reportAbuse(
+        body={
+            "reasonId": "N",
+            "videoId": "nFHbzIZoaxE"
+        }
+    )
+    request.execute()
+    if request.status_code == 204:
+        print("Reported")
+    else:
+        print("Error")
+
+
+if __name__ == "__main__":
+    main()
